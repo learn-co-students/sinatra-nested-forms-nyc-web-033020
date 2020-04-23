6e53881fcb2b6b0e4b8bcaf578b5a83a6b258194
byoon97 <byoon5397@gmail.com>
2020-04-23 03:47:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require './environment'
-
+require 'logger'
+logger = Logger.new("log.txt")
+logger.debug "App is started"
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      logger.debug "Creating new pirate"
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      logger.debug "pirate name: #{params[:pirate][:name]}"
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |info|
+        Ship.new(info)
+      end
+      @ships = Ship.all
+
+      @test = "test"
+
+      erb :'pirates/show'
+    end
   end
+
 end
